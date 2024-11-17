94c4455f69dc495ff27b2b1187d11a23a7b4b7dd
AbelBusi <cesarabelbustamante@gmail.com>
2024-11-17 14:25:38 -0500

    Se agrego la vista para observar todas las ventas realizadas

--- src/main/java/com/mycompany/farmaciasaludproyecto/view/menu/InterGestionarVentas.java
+++ src/main/java/com/mycompany/farmaciasaludproyecto/view/menu/InterGestionarVentas.java
@@ -43,9 +43,9 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        txt_fecha = new javax.swing.JTextField();
-        jComboBox_cliente = new javax.swing.JComboBox<>();
+        txt_cliente = new javax.swing.JTextField();
         jComboBox_estado = new javax.swing.JComboBox<>();
+        txt_fecha1 = new javax.swing.JTextField();
         jLabel_wallpaper = new javax.swing.JLabel();
 
         setClosable(true);
@@ -54,8 +54,9 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos-humanos.png"))); // NOI18N
         jLabel1.setText("Administrar Ventas");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 20, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 10, -1, -1));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
@@ -84,13 +85,15 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
 
         jButton_actualizar.setBackground(new java.awt.Color(51, 204, 0));
         jButton_actualizar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton_actualizar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/rotacion.png"))); // NOI18N
         jButton_actualizar.setText("Actualizar");
+        jButton_actualizar.setContentAreaFilled(false);
         jButton_actualizar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton_actualizarActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton_actualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
+        jPanel2.add(jButton_actualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, -1));
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 50, 130, 270));
 
@@ -100,39 +103,48 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ventas.png"))); // NOI18N
         jLabel2.setText("Total Pagar:");
-        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 90, -1));
+        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 120, -1));
 
         txt_total_pagar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         txt_total_pagar.setEnabled(false);
-        jPanel3.add(txt_total_pagar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 10, 170, -1));
+        jPanel3.add(txt_total_pagar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 10, 170, -1));
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/fecha-de-caducidad.png"))); // NOI18N
         jLabel3.setText("Fecha:");
-        jPanel3.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 90, -1));
+        jPanel3.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 120, -1));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tarifa.png"))); // NOI18N
         jLabel4.setText("Cliente:");
-        jPanel3.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 10, 90, -1));
+        jPanel3.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 10, 90, -1));
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/actual.png"))); // NOI18N
         jLabel5.setText("Estado:");
-        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 40, 90, -1));
+        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 50, 90, -1));
 
-        txt_fecha.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        txt_fecha.setEnabled(false);
-        jPanel3.add(txt_fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 40, 170, -1));
-
-        jComboBox_cliente.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jComboBox_cliente.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione cliente:", "Item 2", "Item 3", "Item 4" }));
-        jPanel3.add(jComboBox_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 10, 170, -1));
+        txt_cliente.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        txt_cliente.setEnabled(false);
+        jPanel3.add(txt_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 10, 170, -1));
 
         jComboBox_estado.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jComboBox_estado.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Activo", "Inactivo" }));
-        jPanel3.add(jComboBox_estado, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 40, 170, -1));
+        jPanel3.add(jComboBox_estado, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 50, 170, 30));
+
+        txt_fecha1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        txt_fecha1.setEnabled(false);
+        txt_fecha1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_fecha1ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(txt_fecha1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 50, 170, -1));
 
         getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 330, 870, 100));
         getContentPane().add(jLabel_wallpaper, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 890, 470));
@@ -144,10 +156,13 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
 
     }//GEN-LAST:event_jButton_actualizarActionPerformed
 
+    private void txt_fecha1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_fecha1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_fecha1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton_actualizar;
-    private javax.swing.JComboBox<String> jComboBox_cliente;
     private javax.swing.JComboBox<String> jComboBox_estado;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -160,7 +175,8 @@ public class InterGestionarVentas extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel3;
     public static javax.swing.JScrollPane jScrollPane1;
     public static javax.swing.JTable jTable_ventas;
-    private javax.swing.JTextField txt_fecha;
+    private javax.swing.JTextField txt_cliente;
+    private javax.swing.JTextField txt_fecha1;
     private javax.swing.JTextField txt_total_pagar;
     // End of variables declaration//GEN-END:variables
 
